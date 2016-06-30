e51878dac57c4910144d724c5cbf12e3003e6ff8
mjosse <mjosse@student.42.fr>
2016-06-30 11:20:26 +0200

    modif plist ajout fbsdk

--- happer/detailProduitVC.swift
+++ happer/detailProduitVC.swift
@@ -20,7 +20,6 @@ class detailProduitVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
