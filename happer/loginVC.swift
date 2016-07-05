09d97ebbc2225580dbbabb32eeebe2dc53435718
mjosse <mjosse@student.42.fr>
2016-07-05 14:16:30 +0200

    inscription/connexion

--- happer/loginVC.swift
+++ happer/loginVC.swift
@@ -8,19 +8,33 @@
 
 import UIKit
 
-class loginVC: UIViewController {
+class loginVC: UIViewController, UITextFieldDelegate {
 
     // MARK: - properties
     
     @IBOutlet weak var loginField: UITextField!
     @IBOutlet weak var passwdField: UITextField!
+    var jsonData = NSDictionary()
     
     // MARK: - parent.methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loginField.delegate = self
+        self.passwdField.delegate = self
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(loginVC.dismissKeyboard))
+        view.addGestureRecognizer(tap)
 
         // Do any additional setup after loading the view.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,12 +45,67 @@ class loginVC: UIViewController {
     // MARK: - self.methods
     
     @IBAction func submit(sender: UIButton) {
+        if loginField.text == "" || passwdField.text == "" {
+            return
+        }
+        let login = loginField.text!.lowercaseString as NSString
+        let passwd = passwdField.text!.lowercaseString as NSString
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: "http://ec2-52-49-149-140.eu-west-1.compute.amazonaws.com:80/login.php")
+        let body = "login=\(login)&passwd=\(passwd)"
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        let postData: NSData = body.dataUsingEncoding(NSUTF8StringEncoding)!
+        request.HTTPBody = postData
+        let postLength: NSString = String(postData.length)
+        
+        request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        var res = NSHTTPURLResponse()
+        var urlData: NSData?
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            urlData = data! as NSData
+            res = response as! NSHTTPURLResponse
+            if res.statusCode >= 200 && res.statusCode < 300 {
+                do
+                {
+                    self.jsonData = try NSJSONSerialization.JSONObjectWithData(urlData!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary // On convertis en Dictionnaire, ce sera plus facile a lire
+                }
+                catch
+                {
+                    print("Catch-Location:: 'loginPageVC' :: Serialization of server's response <jsonData>")
+                    return
+                }
+            }
+            else {
+                print(res.statusCode)
+                return
+            }
+            print(self.jsonData)
+            let success = self.jsonData.valueForKey("id") as! NSInteger
+            if (success > 0) {
+                NSOperationQueue.mainQueue().addOperationWithBlock {
+                    let story = UIStoryboard.init(name: "Main", bundle: nil)
+                    let vc = story.instantiateViewControllerWithIdentifier("inspiVC")
+                    self.presentViewController(vc, animated: true, completion: nil)
+                }
+            }
+            else {
+                print("FAIL")
+                self.loginField.text = ""
+                self.passwdField.text = ""
+            }
+        })
+        task.resume()
     }
 
     @IBAction func back(sender: UITapGestureRecognizer) {
         let story = UIStoryboard.init(name: "Main", bundle: nil)
         let vc = story.instantiateViewControllerWithIdentifier("mainPage") as! mainPageVC
         self.presentViewController(vc, animated: true, completion: nil)
+        
     }
     
     /*
--- happer/signUpVC.swift
+++ happer/signUpVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class signUpVC: UIViewController {
+class signUpVC: UIViewController, UITextFieldDelegate {
 
     // MARK: - properties
     
@@ -21,17 +21,109 @@ class signUpVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loginField.delegate = self
+        self.mailField.delegate = self
+        self.passwdField.delegate = self
+        self.confpassField.delegate = self
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(signUpVC.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
 
     // MARK: - self.methods
 
-    @IBAction func submit(sender: UIButton) {
-        
+    func checkFields() -> Bool {
+        if loginField.text == "" || mailField.text == "" || passwdField.text == "" || confpassField.text == "" {
+            return false
+        }
+        return true
     }
+    
+    @IBAction func submit(sender: UIButton) {
+        if !checkFields() {
+            loginField.text = ""
+            mailField.text = ""
+            passwdField.text = ""
+            confpassField.text = ""
+        }
+        if passwdField.text!.lowercaseString == confpassField.text!.lowercaseString {
+            print("match")
+        }
+        else {
+            loginField.text = ""
+            mailField.text = ""
+            passwdField.text = ""
+            confpassField.text = ""
+            return
+        }
+        let login = loginField.text!.lowercaseString as NSString
+        let mail = mailField.text!.lowercaseString as NSString
+        let passwd = passwdField.text!.lowercaseString as NSString
+        
+        let session = NSURLSession.sharedSession()
+        var jsonData = NSDictionary()
+        let postr = NSString(string : "login=\(login)&passwd=\(passwd)&mail=\(mail)")
+        let url = NSURL(string: "http://ec2-52-49-149-140.eu-west-1.compute.amazonaws.com:80/adduser.php")
+        let request = NSMutableURLRequest(URL :url!)
+        request.HTTPMethod = "POST"
+        
+        let postData: NSData = postr.dataUsingEncoding(NSUTF8StringEncoding)!
+        request.HTTPBody = postData
+        
+        
+        let postLength: NSString = String(postData.length)
+        
+        
+        request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        var urlData: NSData?
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            urlData = data! as NSData
+            let res = response as! NSHTTPURLResponse
+            if res.statusCode >= 200 && res.statusCode < 300 {
+                do
+                {
+                    jsonData = try NSJSONSerialization.JSONObjectWithData(urlData!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                }
+                catch
+                {
+                    print("Catch-Location:: 'signUpVC' :: Serialization of server's response <jsonData>")
+                    return
+                }
+            }
+            else {
+                print(res.statusCode)
+                return
+            }
+            print(jsonData)
+            let success = jsonData.valueForKey("success") as! NSInteger
+            if (success > 0) {
+                print("SUCCESS")
+                let story = UIStoryboard(name: "Main", bundle: nil)
+                let vc = story.instantiateViewControllerWithIdentifier("mainPage") as! mainPageVC
+                self.presentViewController(vc, animated: true, completion: nil)
+            }
+            else {
+                print("FAIL")
+            }
+        })
+        task.resume()
+    }
+
     @IBAction func back(sender: UITapGestureRecognizer) {
         let story = UIStoryboard.init(name: "Main", bundle: nil)
         let vc = story.instantiateViewControllerWithIdentifier("mainPage") as! mainPageVC
