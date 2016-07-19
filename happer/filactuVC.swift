ed26570c64df426011641ebd78be18490ee84fa2
mjosse <mjosse@student.42.fr>
2016-07-19 18:45:43 +0200

    récupération par catégorie

--- happer/filactuVC.swift
+++ happer/filactuVC.swift
@@ -12,6 +12,7 @@ class filactuVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     //MARK : - attributs
     @IBOutlet weak var mylabel: UILabel!
+    var jsonData = NSDictionary()
 
     var indexSelected: Int = 0
     let catTab: [String] = ["ootd", "ootn", "sacs", "accessoires", "chaussures", "decontracte"]
@@ -20,6 +21,7 @@ class filactuVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.mylabel.text = catTab[indexSelected]
+        getSelfies()
 
         // Do any additional setup after loading the view.
     }
@@ -36,7 +38,58 @@ class filactuVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
 
+    func getSelfies() {
+        let category = mylabel.text! as NSString
+        
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: "http://ec2-52-49-149-140.eu-west-1.compute.amazonaws.com:80/byCategory.php")
+        
+        let body = "category=\(category)"
+        
+        let request = NSMutableURLRequest(URL: url!)
+        
+        request.HTTPMethod = "POST"
+        
+        let postData : NSData = body.dataUsingEncoding(NSUTF8StringEncoding)!
+        request.HTTPBody = postData
+        
+        let postLength: NSString = String(postData.length)
+        
+        request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        var res = NSHTTPURLResponse()
+        var urlData: NSData?
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            urlData = data! as NSData
+            res = response as! NSHTTPURLResponse
+            if res.statusCode >= 200 && res.statusCode < 300
+            {
+                do
+                {
+                    self.jsonData = try NSJSONSerialization.JSONObjectWithData(urlData!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                }
+                catch
+                {
+                    print("Erreur dans le catch sur FilActu")
+                    return
+                }
+            }
+            else
+            {
+                print("Erreur, statusCode : ")
+                print(res.statusCode)
+                return
+            }
+            print(self.jsonData)
+        })
+        task.resume()
     }
 
     /*
