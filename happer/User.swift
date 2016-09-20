ccf36200a97a396e6ced16a80a1c629d7a01d082
charlesfournier <charladfr@me.com>
2016-09-20 16:59:16 +0200

    Create User model

---
+++ happer/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  happer
+//
+//  Created by Josse on 29/06/2016.
+//  Copyright © 2016 mjosse. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class User : Object {
+    
+    dynamic var id = 0
+    dynamic var email = ""
+    dynamic var exp = 0.0
+    dynamic var credit = 0
+    
+    let selfies = List<Selfie>()
+    
+    dynamic var circleRawValue = Circle.Unknown.rawValue
+    var circle : Circle {
+        get {
+            return Circle(rawValue: circleRawValue)!
+        }
+        set {
+            circleRawValue = newValue.rawValue
+        }
+    }
+}
