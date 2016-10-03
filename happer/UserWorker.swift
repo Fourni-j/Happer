54137190e5e74106b77c8a82dd16152bc93e3b28
charlesfournier <charladfr@me.com>
2016-10-03 10:49:13 +0200

    User worker done

--- happer/UserWorker.swift
+++ happer/UserWorker.swift
@@ -10,86 +10,49 @@ import Foundation
 import Future
 
 class UserWorker {
+
+    static func insert(users: [User]){
+        for user in users {
+            UserWorker.insert(user)
+        }
+    }
     
-    static func insert(data: AnyObject) -> Future<[User]> {
-        let users = dictionaryFromArray(data as! [[String: AnyObject]])
+    static func insert(user: User) {
+        DAL.sharedInstance.add(user)
+    }
+    
+    static func parse(data: AnyObject) -> Future<[User]> {
         let future = Future<[User]>()
+        var users = [User]()
+        let array = data as! [[String: AnyObject]]
+
+        for userJSON in array {
+            
+            guard let id = userJSON["id"] as? Int,
+                let email = userJSON["email"] as? String,
+                let exp = userJSON["experience"] as? Double,
+                let credit = userJSON["credit"] as? Int,
+                let circle = userJSON["circle"] as? String,
+                let _ = userJSON["selfies"] as? [String: AnyObject]
+                else {
+                    print("Something goes wrong ==> User")
+                    future.reject()
+                    return future
+            }
+
+            let user = User()
+            user.id = id
+            user.email = email
+            user.exp = exp
+            user.credit = credit
+            user.circle = Circle.init(value: circle)
+
+            //SelfieWorker.parse(selfies)
+            // .then { selfies in }
+            
+            users.append(user)
+        }
         future.resolve(users)
         return future
     }
-    
-    private static func dictionaryFromArray(array: [[String: AnyObject]]) -> [User] {
-        
-        var users = [User]()
-        
-        for json in array {
-            if let user = userFromDictionary(json) {
-                users.append(user)
-            }
-        }
-        return users
-    }
-    
-    private static func userFromDictionary(json: [String: AnyObject]) -> User? {
-        
-        guard let id = json["id"] as? Int,
-            let email = json["email"] as? String,
-            let exp = json["experience"] as? Double,
-            let credit = json["credit"] as? Int,
-            let circle = json["circle"] as? String,
-            let selfies = json["selfies"] as? [String: AnyObject]
-            else {
-                print("Something goes wrong ==> User")
-                return nil
-        }
-        
-        //        print("----------USER----------")
-        //        print("ID : ", id)
-        //        print("email : ", email)
-        //        print("exp : ", exp)
-        //        print("credit : ", credit)
-        //        print("circle : ", circle)
-        //        print("------------------------")
-        
-        let user = User()
-        user.id = id
-        user.email = email
-        user.exp = exp
-        user.credit = credit
-        user.circle = Circle.init(value: circle)
-        
-        for (_, value) in selfies {
-            
-            guard let idSelfie = value["id"] as? Int,
-                let nbLike = value["nb_like"] as? Int,
-                let rating = value["rating"] as? Int,
-                let imgURL = value["url_image"] as? String,
-                let categorySelfie = value["category"] as? String
-                else {
-                    print("Something goes wrong ==> Selfie")
-                    return nil
-            }
-            
-            //            print("---------SELFIE---------")
-            //            print("idSelfie : ", idSelfie)
-            //            print("nbLike : ", nbLike)
-            //            print("rating : ", rating)
-            //            print("imgURL : ", imgURL)
-            //            print("category : ", categorySelfie)
-            //            print("------------------------")
-            
-            let selfie = Selfie()
-            selfie.id = idSelfie
-            selfie.nbLike = nbLike
-            selfie.rating = rating
-            selfie.imageURLString = imgURL
-            selfie.owner = user
-            selfie.category = Selfie.Category.init(value: categorySelfie)
-            user.selfies.append(selfie)
-        }
-        //        print(user)
-        return user
-    }
-    
-    
 }
