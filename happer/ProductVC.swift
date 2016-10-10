1c5277075bdb59d278b87eddb676b7f1afad1787
charlesfournier <charladfr@me.com>
2016-10-10 16:10:13 +0200

    Build ViewController from specific circle

--- happer/ProductVC.swift
+++ happer/ProductVC.swift
@@ -8,72 +8,84 @@
 
 import UIKit
 import MagicSwiftBus
+import RealmSwift
 
 class ProductVC: UIViewController {
-
-    var currentCircle = Circle.Unknown
-    var productInteractor = ProductInteractor()
+    
+    @IBOutlet weak var productCollectionView: UICollectionView!
+    
+    var resultProducts: Results<Product>!
+    var selectedProduct: Product!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        productCollectionView.delegate = self
+        productCollectionView.dataSource = self
     }
-
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        self.title = currentCircle.rawValue
-        ProductPresenter.register(self, events: .GetProductSuccess, .GetProductFailure, .BidSuccess, .BidFailure)
-        productInteractor.getProducts()
     }
     
     override func viewDidDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
-        ProductPresenter.unregisterAll(self)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
-
-
-extension ProductVC : ProductEvent {
-
-    func getProductSuccess() {
-        print("Get Product Success")
-        print(DAL.sharedInstance.readProduct(byId: 42))
-    }
-    
-    func getProductFailure(error: NSError) {
-        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
-        let okAction = UIAlertAction(title: "Ok", style: .Default) { (action:UIAlertAction!) in
-            self.navigationController?.popViewControllerAnimated(true)
+    func buildFromCircle(circle: Circle) {
+        resultProducts = DAL.sharedInstance.readProduct(byCircle: circle)
+        if productCollectionView != nil {
+            productCollectionView.reloadData()
         }
-        alertController.addAction(okAction)
-        self.presentViewController(alertController, animated: true, completion: nil)
     }
     
-    func bidSuccess() {
-        
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let destinationVC = segue.destinationViewController as? ProductDetailsVC {
+            destinationVC.selectedProduct = selectedProduct
+        }
+    }
+}
+
+extension ProductVC : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if (resultProducts != nil) {
+            return resultProducts.count
+        }
+        return 0
     }
     
-    func bidFailure(error: NSError) {
-        
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("productCollectionViewCell", forIndexPath: indexPath) as! ProductCollectionViewCell
+        cell.backgroundColor = UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.00)
+        cell.brandLabel.text = resultProducts[indexPath.row].brand
+        cell.descriptionLabel.text = resultProducts[indexPath.row].desc
+        cell.priceLabel.text = "Price : \(resultProducts[indexPath.row].price)€"
+        cell.imageView.af_setImageWithURL(resultProducts[indexPath.row].imageURL)
+        cell.cellState = resultProducts[indexPath.row].state
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionElementKindSectionHeader :
+            let headerView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "productHeader", forIndexPath: indexPath)
+            return headerView
+        default:
+            assert(false, "unexpected element kind")
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 200.0)
+    }
+
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        selectedProduct = resultProducts[indexPath.row]
+        Session.sharedInstance.router.perform("route://Product/productDetailsVC#push", sender: self)
     }
     
 }
-
