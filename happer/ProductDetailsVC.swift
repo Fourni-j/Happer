e6ed041b893a750e8e68b8ca7c53f7d6f9de4a75
charlesfournier <charladfr@me.com>
2016-10-10 16:08:12 +0200

    basic productDetailsVC

--- happer/ProductDetailsVC.swift
+++ happer/ProductDetailsVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ProductDetailsVC: UIViewController {
 
+    var selectedProduct: Product!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,16 +23,11 @@ class ProductDetailsVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidAppear(animated: Bool) {
+        print("selected product : \(selectedProduct)")
     }
-    */
-
+ 
 }
 
 extension ProductDetailsVC : ProductEvent {
