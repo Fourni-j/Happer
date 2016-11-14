e712991a9ab3f39a68a86ae2f4c24f5a2a0fcc49
charlesfournier <charladfr@me.com>
2016-11-14 15:56:54 +0100

    remove useless properties and methods

--- happer/NewsFeedVC.swift
+++ happer/NewsFeedVC.swift
@@ -9,46 +9,47 @@
 import UIKit
 import Foundation
 import AlamofireImage
+import RealmSwift
 
 class NewsFeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    //MARK : - attributs
-    
-    var jsonData = NSDictionary()
-    
     @IBOutlet weak var table: UITableView!
-    var indexSelected = 0
-    let catTab = ["Ootd", "Ootn", "Sacs", "Accessoires", "Chaussures", "Decontracte"]
-    var selfieTab: [Selfie] = []
     
-    // transmission
-    
-    var toTransmit = Selfie()
+    var resultSelfies: Results<Selfie>!
+    var newsFeedInteractor = NewsFeedInteractor()
+    var currentCategory = Selfie.Category.init(value: "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(InspirationVC.moveToHappLike), name: "happLike", object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(InspirationVC.moveToShare), name: "share", object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(InspirationVC.moveToFriends), name: "friends", object: nil)
         
-        title = catTab[indexSelected]
-        let url = NSURL(string: "http://ec2-52-49-149-140.eu-west-1.compute.amazonaws.com:80/get\(catTab[indexSelected]).php")
-        getSelfies(url!)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        NewsFeedPresenter.register(self, events: .GetSelfiesSuccess, .GetSelfiesFailure)
+        newsFeedInteractor.getSelfies()
+        title = currentCategory.value
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        NewsFeedPresenter.unregisterAll(self)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    // MARK: - tableView methods
-    
+
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return selfieTab.count
+        if resultSelfies != nil {
+            return resultSelfies.count
+        }
+        return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.table.dequeueReusableCellWithIdentifier("newsFeedCell", forIndexPath: indexPath) as! NewsFeedCell
-        let selfie = selfieTab[indexPath.row]
+        let selfie = resultSelfies[indexPath.row]
         cell.cellRating.rating = Float(selfie.rating)
         cell.cellImage.af_setImageWithURL(selfie.imageURL)
         cell.cellLikeCount.text = "\(selfie.nbLike)"
@@ -63,39 +64,35 @@ class NewsFeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         cell.cellImage.tagHidden = !cell.cellImage.tagHidden
         tableView.deselectRowAtIndexPath(indexPath, animated: false)
     }
+}
+
+extension NewsFeedVC : NewsFeedEvent {
     
-    func getSelfies(url: NSURL) {
-        let jsonData = NSData(contentsOfURL: url)
-        var result = NSDictionary()
-        do {
-            result = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+    func getSelfiesSuccess() {
+        resultSelfies = DAL.sharedInstance.readSelfie(byCategory: currentCategory)
+        
+        if resultSelfies.count == 0 {
+            let alertController = UIAlertController(title: "Empty category", message: "Try again later", preferredStyle: .Alert)
+            let okAction = UIAlertAction(title: "Ok", style: .Default) {
+                (action: UIAlertAction!) in
+                self.navigationController?.popViewControllerAnimated(true)
+            }
+            alertController.addAction(okAction)
+            presentViewController(alertController, animated: true, completion: nil)
         }
-        catch {
-            print("Error catched, func getSelfie() in filactuVC.swift")
-            return
-        }
-        let tab = result.valueForKey("content") as! NSDictionary
-        var new: Selfie
-        for val in tab {
-            new = Selfie()
-            new.id = val.value["id"] as! Int
-            new.category = Selfie.Category.init(value: self.catTab[indexSelected] as String)
-            new.nbLike = val.value["nbLike"] as! Int
-            new.state = Selfie.State.Unknown
-            new.rating = val.value["rate"] as! Int
-            new.imageURLString = val.value["url"] as! String
-            selfieTab += [new]
-        }
+        
+        table.reloadData()
+        print(resultSelfies)
     }
     
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "goToInspi" {
-            let destination = segue.destinationViewController as! SelfieDetailsVC
-            destination.selfie = self.toTransmit
-            destination.indexSelected = self.indexSelected
+    func getSelfiesFailure(error: NSError) {
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "Ok", style: .Default) {
+            (action: UIAlertAction!) in
+            self.navigationController?.popViewControllerAnimated(true)
         }
+        alertController.addAction(okAction)
+        presentViewController(alertController, animated: true, completion: nil)
     }
+    
 }
