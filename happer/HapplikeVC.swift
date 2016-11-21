3141244fa9446527e9633f67ce93c5850bbec5d8
charlesfournier <charladfr@me.com>
2016-11-21 18:01:57 +0100

    basic setup and design

--- happer/HapplikeVC.swift
+++ happer/HapplikeVC.swift
@@ -10,41 +10,40 @@ import UIKit
 import Foundation
 
 class HapplikeVC: UIViewController {
-
-    // MARK : - NSUserDefault
     
-    var cache = NSUserDefaults.standardUserDefaults()
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var expView: UIView!
+    @IBOutlet weak var notificationButton: UIButton!
     
-    // MARK : - attributs
+    var fillView: UIView?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        cache.setObject("happLikeVC", forKey: "currentVC")
-
-        // Do any additional setup after loading the view.
+        
+        imageView.af_setImageWithURL(NSURL(string: "http://s3.eu-central-1.amazonaws.com/happer/selfies/pictures/000/000/013/original/IMG_8229.JPG?1479134155")!)
+        fillExp(3, neededExp: 4)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
-    @IBAction func backButton(sender: UIButton) {
-        let story = UIStoryboard.init(name: "Main", bundle: nil)
-        let vc = story.instantiateViewControllerWithIdentifier(cache.objectForKey("prevVC") as! String)
-        cache.setObject("happLikeVC", forKey: "prevVC")
-        self.presentViewController(vc, animated: true, completion: nil)
-
+    
+    func fillExp(currentExp: Int, neededExp: Int) {
+        
+        let percentage: CGFloat = CGFloat(currentExp) / CGFloat(neededExp)
+        
+        if fillView == nil {
+            fillView = UIView(frame: CGRectMake(0, 0, expView.frame.width * percentage, expView.frame.height))
+            fillView?.backgroundColor = UIColor(red:0.96, green:0.83, blue:0.21, alpha:1.00)
+            expView.addSubview(fillView!)
+        } else {
+            fillView?.frame = CGRectMake(0, 0, expView.frame.width * percentage, expView.frame.height)
+        }
     }
-
+    
+    @IBAction func notificationAction(sender: AnyObject) {
+    }
+    
 }
