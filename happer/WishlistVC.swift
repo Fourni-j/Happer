6759fdc0a3e91984d3dfe47f2896261951d8cc50
PaulGuillot <paulguillot@live.fr>
2016-11-25 13:23:40 +0100

    adding reference happer/WishlistVC.swift

---
+++ happer/WishlistVC.swift
@@ -0,0 +1,36 @@
+//
+//  WishlistVC.swift
+//  happer
+//
+//  Created by paul on 25/11/2016.
+//  Copyright © 2016 mjosse. All rights reserved.
+//
+
+import UIKit
+
+class WishlistVC: UIViewController {
+
+    //lol it's the good one
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
