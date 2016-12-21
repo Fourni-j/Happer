63eab27d4f8e6653e4d687dd92eb56156bac302e
Charles Fournier <charladfr@me.com>
2016-12-21 15:40:19 +0100

    Facebook user model for ask friend help

---
+++ happer/FacebookUsers.swift
@@ -0,0 +1,21 @@
+//
+//  FacebookUsers.swift
+//  happer
+//
+//  Created by Charles Fournier on 21/12/2016.
+//  Copyright © 2016 mjosse. All rights reserved.
+//
+
+import UIKit
+
+class FacebookUsers: NSObject {
+    var name = ""
+    var picturesString = ""
+    var id = ""
+    var isSelected = false
+}
+
+
+
+
+
