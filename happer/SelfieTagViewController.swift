13f806e23f1baf9c65f709cbfb5a6e0517dd4d14
Charles Fournier <charladfr@me.com>
2016-12-21 17:06:39 +0100

    change behaviour to add brand tag view controller

--- happer/SelfieTagViewController.swift
+++ happer/SelfieTagViewController.swift
@@ -24,7 +24,7 @@ class SelfieTagViewController: UIViewController {
     }
     
     @IBAction func saveAction(sender: AnyObject) {
-        self.navigationController?.popToRootViewControllerAnimated(false)
+        Session.sharedInstance.router.perform("route://Happies/brandTagViewController#push")
     }
  }
 
